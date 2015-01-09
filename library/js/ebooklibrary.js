cb4e88353e806c7f94e8637803389189f7ff0bc9
Adrian Elgar <ade@mrdragonraaar.com>
2015-01-09 01:30:13 +0000

    built v2

--- library/js/ebooklibrary.js
+++ library/js/ebooklibrary.js
@@ -16,9 +16,7 @@ requirejs.config({
 		Handlebars: 'lib/hbs/Handlebars',
 		i18nprecompile: 'lib/hbs/i18nprecompile',
 		json2: 'lib/hbs/json2',
-		bootstrap: 'lib/bootstrap/bootstrap.min',
-		//touch: 'lib/touch/jquery-simple-touch.min'
-		touch: 'lib/touch/jquery-simple-touch'
+		bootstrap: 'lib/bootstrap/bootstrap.min'
 	},
 	hbs:
 	{
