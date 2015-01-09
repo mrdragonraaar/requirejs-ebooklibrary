10194862e0b5baf8bba6c2855c29c21f644e234c
Adrian Elgar <ade@mrdragonraaar.com>
2015-01-09 00:34:45 +0000

    removed links dropdown

--- library/js/ebooklibrary/view/navbar/NavBarView.js
+++ library/js/ebooklibrary/view/navbar/NavBarView.js
@@ -9,26 +9,20 @@ define([
     'hbs!ebooklibrary/template/navbar/NavBarView',
     'ebooklibrary/view/navbar/NavBarBreadcrumbView',
     'ebooklibrary/view/navbar/NavBarSearchBoxView',
-    'ebooklibrary/view/navbar/NavBarNavToolBarView',
-    'ebooklibrary/view/navbar/NavBarLinkDropdownView',
     'backbone'
 ],
 function(
     NavBarViewTemplate,
     NavBarBreadcrumbView,
     NavBarSearchBoxView,
-    NavBarNavToolBarView,
-    NavBarLinkDropdownView,
     Backbone
 ) {
 	var NavBarView = Backbone.View.extend({
 		tagName: 'nav',
-		className: 'navbar navbar-default !navbar-fixed-top navbar-static-top',
+		className: 'navbar navbar-default navbar-static-top',
 
 		breadcrumbView: null,	// breadcrumb view
 		searchBoxView: null,	// search box view
-		navToolBarView: null,	// nav toolbar view
-		//linkDropdownView: null,	// link dropdown view
 
 		/**
 		 * Initialise the navigation bar view.
@@ -39,8 +33,6 @@ function(
 
 			this.breadcrumbView = new NavBarBreadcrumbView();
 			this.searchBoxView = new NavBarSearchBoxView();
-			//this.navToolBarView = new NavBarNavToolBarView();
-			//this.linkDropdownView = new NavBarLinkDropdownView();
 		},
 
 		/**
@@ -51,13 +43,7 @@ function(
 			var navBarTmpl = NavBarViewTemplate();
 			this.$el.html(navBarTmpl);
 
-			//this.$('.container-fluid').prepend(this.breadcrumbView.render().el);
 			this.$('.navbar-collapse').prepend(this.breadcrumbView.render().el);
-
-			//this.$('.navbar-collapse').append(this.linkDropdownView.render().el);
-			//this.$('.navbar-collapse').append(this.navToolBarView.render().el);
-
-			//this.$('.container-fluid').append(this.searchBoxView.render().el);
 			this.$('.navbar-collapse').append(this.searchBoxView.render().el);
 
 			return this;
